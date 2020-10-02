ccbbed678748f13194abe5aa13faad9437cd0497
nelson-singleton <nsingleton@ymail.com>
2020-10-02 11:28:28 -0500

    Fixed logic on getting actions for specific projects.

--- actionsRouter.js
+++ actionsRouter.js
@@ -1,6 +1,59 @@
 const express = require('express')
-const actionsRouter = express.Router()
+const router = express.Router()
 const actionsDb = require('./data/helpers/actionModel')
+const projectsDb = require('./data/helpers/projectModel')
+
+router.get('/', (req, res) => {
+    actionsDb.get()
+    .then (actions => {
+        res.status(200).json(actions)
+    })
+    .catch(error => {
+        res.status(500).json({error: "No actions found"})
+    })
+})
+
+router.get('/:id', (req, res) => {
+    //const checkId = projectsDb.getProjectActions(req.params.id)
+    projectsDb.getProjectActions(req.params.id)
+    .then(actions => {
+     if(actions.project_id == req.params.id) {res.status(200).json(actions)}
+     else if (actions.project_id != req.params.id) {res.status(400).json({message: "Could not find actions associated with that project"})}
+     else {res.status(400).json({error: "Error. Try again."})}         
+    }) 
+   
+})
+
+router.post('/', (req, res) => {
+    actionsDb.insert(req.body)
+    .then(actions => {
+        res.status(201).json(actions)
+    })
+    .catch(error => res.status(400).json(error))
+})
+
+router.put('/:id', (req, res) => {
+    actionsDb.update(req.params.id, req.body)
+    .then(actions => {
+        if(change){
+            res.status(200).json(actopms)
+        } else {res.status(404).json({message: "Could not find that post"})}
+    })
+    .catch(error => {
+        console.log(error)
+        res.status(500).json({message: "Error updating. Try again."})
+    })
+})
+
+router.delete('/:id', (req, res) => {
+    actionsDb.remove(req.params.id)
+    .then(actions => {
+        if(item == 0){
+            res.status(404).json({error: "Could not find that ID"})
+       } else{res.status(200).json({message: "The post has been deleted"})}
+    })
+    .catch(error => res.status(500).json({message:"The post could not be removed"}))
+})
 
 
 
@@ -8,4 +61,5 @@ const actionsDb = require('./data/helpers/actionModel')
 
 
 
-module.exports = actionsRouter;
+
+module.exports = router;
--- projectsRouter.js
+++ projectsRouter.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const router = express.Router()
 const projectsDb = require('./data/helpers/projectModel')
-
+//get all
 router.get('/', (req, res) => {
     projectsDb.get()
     .then (projects => {
@@ -12,7 +12,7 @@ router.get('/', (req, res) => {
         res.status(500).json({error: "No projects found"})
     })
 })
-
+//get by id
 router.get('/:id', (req, res) => {
     projectsDb.get(req.params.id)
     .then(project => {
@@ -21,7 +21,17 @@ router.get('/:id', (req, res) => {
         } else {res.status(404).json({message: "Project with that ID not found"})}
     })
 })
-
+// get actions by id
+router.get('/:id/actions', (req, res) => {
+    projectsDb.getProjectActions(req.params.id)
+    .then (projectActions => {
+        res.status(200).json(projectActions)
+    })
+    .catch(error => {
+        res.status(500).json({error: "No projects found"})
+    })
+})
+//post project
 router.post('/', (req, res) => {
     projectsDb.insert(req.body)
     .then(projects => {
@@ -29,7 +39,7 @@ router.post('/', (req, res) => {
     })
     .catch(error => res.status(400).json(error))
 })
-
+//change project
 router.put('/:id', (req, res) => {
     projectsDb.update(req.params.id, req.body)
     .then(change => {
@@ -42,7 +52,7 @@ router.put('/:id', (req, res) => {
         res.status(500).json({message: "Error updating. Try again."})
     })
 })
-
+//delete project
 router.delete('/:id', (req, res) => {
     projectsDb.remove(req.params.id)
     .then(item => {
@@ -54,6 +64,6 @@ router.delete('/:id', (req, res) => {
 })
 
 
-//update and remove
+
 
 module.exports = router;
